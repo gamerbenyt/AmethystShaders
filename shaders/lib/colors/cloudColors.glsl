894a6c103481990a3a20414e4644a77049645961
Gamer Ben <136914442+gamerbenyt@users.noreply.github.com>
2026-07-30 10:49:59 -0400

    Added back White glowing clouds

--- shaders/lib/colors/cloudColors.glsl
+++ shaders/lib/colors/cloudColors.glsl
@@ -1,7 +1,7 @@
 vec3 cloudRainColor = mix(nightMiddleSkyColor, dayMiddleSkyColor, sunFactor);
-vec3 cloudAmbientColor = mix(ambientColor * (sunVisibility2 * (0.55 + 0.17 * noonFactor) + 0.35), cloudRainColor * 0.5, rainFactor);
+vec3 cloudAmbientColor = mix(ambientColor * (sunVisibility2 * (1.55 + 0.1 * noonFactor) + 0.35), cloudRainColor * 0.5, rainFactor);
 vec3 cloudLightColor   = mix(
-    lightColor * 3.9,
+    lightColor * (3.9 + 0.2 * noonFactor + 1.75),
     cloudRainColor * 0.25,
     noonFactor * rainFactor
 );
