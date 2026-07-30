27b90095ddbbf7d794d96407224188497a815ea7
Gamer Ben <136914442+gamerbenyt@users.noreply.github.com>
2026-07-30 10:51:12 -0400

    Fix Sky Colors

--- shaders/lib/colors/skyColors.glsl
+++ shaders/lib/colors/skyColors.glsl
@@ -10,7 +10,7 @@
 
         #ifdef SPECIAL_BIOME_WEATHER
             vec3 nmscSnowM = inSnowy * vec3(-0.3, 0.05, 0.2);
-            vec3 nmscDryM = inDry * vec3(0.3);
+            vec3 nmscDryM = inDry * vec3(-0.3);
             vec3 ndscSnowM = inSnowy * vec3(-0.25, -0.01, 0.25);
             vec3 ndscDryM = inDry * vec3(-0.05, -0.09, -0.1);
         #else
@@ -29,12 +29,11 @@
         #else
             vec3 nmscRainM = vec3(0.0), ndscRainM = vec3(0.0);
         #endif
-        vec3 nuscWeatherM = vec3(0.1, 0.0, 0.1);
         vec3 nmscWeatherM = vec3(-0.1, -0.4, -0.6) + vec3(0.0, 0.06, 0.12) * noonFactor;
         vec3 ndscWeatherM = vec3(-0.15, -0.3, -0.42) + vec3(0.0, 0.02, 0.08) * noonFactor;
 
-        vec3 noonUpSkyColor     = pow(skyColorM, vec3(7.9)) * (vec3(0.85, 0.92, 0.81) + rainFactor * nuscWeatherM);
-        vec3 noonMiddleSkyColor = pow(skyColorM, vec3(0.75)) * (vec3(1.3) + rainFactor * (nmscWeatherM + nmscRainM + nmscSnowM + nmscDryM))
+        vec3 noonUpSkyColor     = pow(skyColorM, vec3(7.9)); 
+        vec3 noonMiddleSkyColor = skyColorM * (vec3(0.75) + rainFactor * (nmscWeatherM + nmscRainM + nmscSnowM + nmscDryM))
                                 + noonUpSkyColor * 0.6;
         vec3 noonDownSkyColor   = skyColorM * (vec3(0.161, 0.282, 0.882) + rainFactor * (ndscWeatherM + ndscRainM + ndscSnowM + ndscDryM))
                                 + noonUpSkyColor * 0.25;
